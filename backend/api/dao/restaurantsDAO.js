0e05b4e5ff810ef2e5ff9f2e797459c3b16f9dfb
TJ Taylor <tjtaylorjr@gmail.com>
2021-05-02 17:24:39 -0400

    imported restaurantsDAO to index.js for MongoDB connection

--- backend/api/dao/restaurantsDAO.js
+++ backend/api/dao/restaurantsDAO.js
@@ -6,7 +6,7 @@ export default class RestaurantsDAO {
       return
     }
     try {
-      restaurants = await conn.db(pross.env.REVIEWS_NS).collection("restaurants");
+      restaurants = await conn.db(process.env.REVIEWS_NS).collection("restaurants");
     } catch (err) {
       console.error(`Unable to establish a collection handle in restaurantsDAO: ${err}`);
     };
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import app from "./server.js";
 import mongodb from "mongodb";
 import dotenv from "dotenv";
+import RestaurantsDAO from "./api/dao/restaurantsDAO.js";
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ MongoClient.connect(
     process.exit(1)
   })
   .then(async client => {
+    await RestaurantsDAO.injectDB(client);
     app.listen(port, () => {
       console.log(`listening on port ${port}`);
     });
