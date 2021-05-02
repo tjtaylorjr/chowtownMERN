05eb4ef4b4cf3be4ce4a49e94b87a08b0906a137
TJ Taylor <tjtaylorjr@gmail.com>
2021-05-02 18:13:49 -0400

    added restaurants.controller.  API now functional and pulling data from the database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import app from "./server.js";
 import mongodb from "mongodb";
 import dotenv from "dotenv";
-import RestaurantsDAO from "./api/dao/restaurantsDAO.js";
+import RestaurantsDAO from "./dao/restaurantsDAO.js";
 
 dotenv.config();
 
