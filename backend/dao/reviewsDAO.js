43575a4b639536d2f0e8cf82fd18c9836f683651
TJ Taylor <tjtaylorjr@gmail.com>
2021-05-03 12:34:44 -0400

    created reviewsDAO file in dao directory and added add and update methods

---
+++ backend/dao/reviewsDAO.js
@@ -0,0 +1,61 @@
+import mongodb from "mongodb";
+
+const ObjectId = mongodb.ObjectID;
+
+let reviews;
+
+export default class ReviewsDAO {
+  static async injectDB(conn) {
+    if (reviews) {
+      return;
+    };
+
+    try {
+      reviews = await conn.db(process.env.REVIEWS_NS).collection("reviews");
+    } catch (err) {
+      console.error(`Unable to establish collection handles in userDAO: ${err}`);
+    };
+  };
+
+  static async addReview(restaurantId, user, review, date) {
+    try {
+      const reviewDoc = {
+        name: user.name,
+        user_id: user._id,
+        date: date,
+        text: review,
+        restaurant_id: ObjectId(restaurantId),
+      };
+
+      return await reviews.insertOne(reviewDoc);
+    } catch (err) {
+      console.error(`Unable to post review: ${err}`);
+      return { error: err };
+    };
+  };
+
+  static async updateReview(reviewId, userId, text, date) {
+    try {
+      const updateResponse = await reviews.updateOne(
+        {
+          user_id: userId,
+          _id: ObjectId(reviewId),
+        },
+        {
+          $set:
+            {
+              text: text,
+              date: date,
+            },
+        },
+      );
+
+      return updateResponse;
+    } catch (err) {
+      console.error(`Unable to update review: ${err}`);
+      return { error: err };
+    };
+  };
+
+
+};
