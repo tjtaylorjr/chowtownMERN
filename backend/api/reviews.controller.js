f83349e23e7a1200304820ac6661b17e5a0b6522
TJ Taylor <tjtaylorjr@gmail.com>
2021-05-03 12:20:56 -0400

    added update and delete methods to the reviews.controller.js file

--- backend/api/reviews.controller.js
+++ backend/api/reviews.controller.js
@@ -24,5 +24,47 @@ export default class ReviewsController {
     };
   };
 
+  static async apiUpdateReview(req, res, next) {
+    try {
+      const reviewId = req.body.review_id;
+      const text = req.body.text;
+      const date = new Date();
 
-}
+      const reviewResponse = await ReviewsDAO.updateReview(
+        reviewId,
+        req.body.user_id,
+        text,
+        date,
+      );
+
+      const { error } = reviewResponse;
+      if (error) {
+        res.status(400).json({ error });
+      };
+
+      if (reviewResponse.modifiedCount === 0) {
+        throw new Error(
+          "unable to update review - user may not be original poster",
+        );
+      };
+
+      res.json({ status: "success" });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    };
+  };
+
+  static async apiDeleteReview(req, res, next) {
+    try {
+      const reviewId = req.query.id;
+      const userId = req.body.user_id;
+      const reviewResponse = await ReviewsDAO.deleteReview(
+        reviewId,
+        userId,
+      );
+      res.json({ status: "success" });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    };
+  };
+};
