94d9aa7c6c26efb53e3df1f3547edd1be0a0a2c5
TJ Taylor <tjtaylorjr@gmail.com>
2021-05-10 12:06:24 -0400

    added some fetch functions in a services file

---
+++ frontend/src/services/restaurant.js
@@ -0,0 +1,67 @@
+
+export const getAllRestaurants = async(page = 0) => {
+  const res = await fetch(`/api/v1/restaurants?page=${page}`,{
+    "Content-type": "application/json",
+  });
+  const payload = await res.json();
+  return payload;
+}
+
+export const getRestaurantById = async (id) => {
+  const res = await fetch(`/api/v1/restaurants/id/${id}`, {
+    "Content-type": "application/json",
+  });
+  const payload = await res.json();
+  return payload;
+}
+
+export const findRestaurants = async (query, by, page = 0) => {
+  const res = await fetch(`/api/v1/restaurants?${by}=${query}&page=${page}`, {
+    "Content-type": "application/json",
+  });
+  const payload = await res.json();
+  return payload;
+}
+
+export const postReview = async (data) => {
+  const { placeholder } = data;
+  const res = await fetch(`/api/v1/restaurants/review`, {
+    method: "POST",
+    "Content-type": "application/json",
+    body: {
+      //placeholder: placeholder,
+    },
+  });
+  const payload = await res.json();
+  return payload;
+}
+
+export const updateReview = async (data) => {
+  const { placeholder } = data;
+  const res = await fetch(`/api/v1/restaurants/review`, {
+    method: "PUT",
+    "Content-type": "application/json",
+    body: {
+      //placeholder: placeholder,
+    },
+  });
+  const payload = await res.json();
+  return payload;
+}
+
+export const deleteReview = async (id) => {
+  const res = await fetch(`/api/v1/restaurants/review?id=${id}`, {
+    method: "DELETE",
+    "Content-type": "application/json",
+  });
+  const payload = await res.json();
+  return payload;
+}
+
+export const getCuisines = async (id) => {
+  const res = await fetch(`/api/v1/restaurants/cuisines`, {
+    "Content-type": "application/json",
+  });
+  const payload = await res.json();
+  return payload;
+}
