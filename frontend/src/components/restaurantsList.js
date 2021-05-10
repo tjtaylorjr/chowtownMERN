e1d7fd78aa334ae876214707a3916bfbf400106f
TJ Taylor <tjtaylorjr@gmail.com>
2021-05-10 13:02:06 -0400

    added useState variables to restaurantsList.js

--- frontend/src/components/restaurantsList.js
+++ frontend/src/components/restaurantsList.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { placeholder } from '../services/restaurant.js';
+import { NavLink } from 'react-router-dom';
 
-const RestaurantsList = () => {
+const RestaurantsList = (props) => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [nameQuery, setNameQuery] = useState("");
+  const [zipcodeQuery, setZipcodeQuery] = useState("");
+  const [cuisineQuery, setCuisineQuery] = useState("");
+  const [cuisines, setCuisines] = useState([]);
+
   return (
     <div className="restaurants-list">
       This is a list of restaurants.
