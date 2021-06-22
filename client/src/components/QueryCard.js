336221e6eb6d56ba721cad49f1193b1dffc2af55
TJ Taylor <tjtaylorjr@gmail.com>
2021-06-22 12:38:22 -0400

    changed search result display pre-api implimentation

--- client/src/components/QueryCard.js
+++ client/src/components/QueryCard.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { FaMapMarkerAlt } from 'react-icons/fa';
 
 const QueryCard = (props) => {
   const { address, cuisine, name, _id } = props.restaurant;
@@ -11,12 +12,25 @@ const QueryCard = (props) => {
     <>
       <div className="query-card">
         <div className="query-card__wrapper">
+          <div className="query-card__image-container"></div>
           <div className="query-card__data-container">
-            <h5 className="query-card__location-name">{name}</h5>
-            <p className="query-card__location-text">
-              <strong>Cuisine: </strong>{cuisine}<br />
-              <strong>Address: </strong>{formattedAddress}
-            </p>
+            <div className="query-card__location-details">
+              <h5 className="query-card__location-name">{name}</h5>
+              <p className="query-card__location-text">
+                <div>
+                  {formattedAddress + ' '}
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href={"http://www.google.com/maps/place/" + formattedAddress}
+                    className="query-card__map-link2"
+                  >
+                    <FaMapMarkerAlt />
+              </a>
+                </div>
+                {cuisine}
+              </p>
+            </div>
             <div className="query-card__link-container">
               <NavLink
                 to={"/restaurants/" + _id}
@@ -24,14 +38,14 @@ const QueryCard = (props) => {
               >
                 Read Reviews
               </NavLink>
-              <a
+              {/* <a
                 target="_blank"
                 rel="noreferrer"
                 href={"http://www.google.com/maps/place/" + formattedAddress}
                 className="query-card__map-link"
               >
                 View Map
-              </a>
+              </a> */}
             </div>
           </div>
         </div>
