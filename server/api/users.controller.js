02e836791be91fcbd164af36ecf0d34a2351f4bc
TJ Taylor <tjtaylorjr@gmail.com>
2021-06-23 13:08:00 -0400

    initial code structure for backend login post in place

--- server/api/users.controller.js
+++ server/api/users.controller.js
@@ -1,10 +1,25 @@
 const UsersDAO = require('../dao/usersDAO.js');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 class UsersController {
   static async apiPostLogin(req, res, next) {
     const { email, password } = req.body;
 
     try {
+      const userRecord = await UsersDAO.getUserByEmail({ email });
+
+      if(!userRecord) {
+        return res.status(404).json({message: "User does not exist"});
+      }
+
+      const passwordMatch = await bcrypt.compare(password, userRecord.password);
+
+      if(!passwordMatch) return res.status(400).json({message: "Invalid credentials"})
+
+      const token = jwt.sign({email: userRecord.email, id: userRecord._id}, secret, { expiresIn: "1h" });
+
+      res.status(200).json({ result: userRecord, token });
 
     } catch (err) {
       console.error(`api, ${err}`);
