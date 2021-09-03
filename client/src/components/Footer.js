bace2a29ec661f112bf61cecabff9b8179464b11
TJ Taylor <tjtaylorjr@gmail.com>
2021-09-03 11:19:40 -0400

    cleanup commented out code

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,19 +3,12 @@ import { FaGithub, FaAngellist, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
 const Footer = () => {
 
-  // const angel = "https://angel.co/u/tj-taylor-2";
-  // const linkedin = "https://www.linkedin.com/in/tj-taylor-6b9412196/";
-  // const github = "https://github.com/tjtaylorjr";
-  // const portfolio = "https://tjtaylorjr.github.io/";
-  // const email = "softwaredevelopmentbytj@gmail.com";
-
   const angel = process.env.REACT_APP_ANGEL;
   const linkedin = process.env.REACT_APP_LINKEDIN;
   const github = process.env.REACT_APP_GITHUB;
   const portfolio = process.env.REACT_APP_PORTFOLIO;
   const email = process.env.REACT_APP_EMAIL;
 
-  console.log(email);
   return (
     <div className="footer">
       {/* {footer_gradient} */}
