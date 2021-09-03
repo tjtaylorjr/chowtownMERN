77144b207d02226f3e82272edd67a30fc7392637
TJ Taylor <tjtaylorjr@gmail.com>
2021-09-03 10:59:42 -0400

    configure package.json serverside for build instructions

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,12 +3,19 @@ import { FaGithub, FaAngellist, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
 const Footer = () => {
 
-  const angel = "https://angel.co/u/tj-taylor-2";
-  const linkedin = "https://www.linkedin.com/in/tj-taylor-6b9412196/";
-  const github = "https://github.com/tjtaylorjr";
-  const portfolio = "https://tjtaylorjr.github.io/";
-  const email = "softwaredevelopmentbytj@gmail.com";
+  // const angel = "https://angel.co/u/tj-taylor-2";
+  // const linkedin = "https://www.linkedin.com/in/tj-taylor-6b9412196/";
+  // const github = "https://github.com/tjtaylorjr";
+  // const portfolio = "https://tjtaylorjr.github.io/";
+  // const email = "softwaredevelopmentbytj@gmail.com";
 
+  const angel = process.env.REACT_APP_ANGEL;
+  const linkedin = process.env.REACT_APP_LINKEDIN;
+  const github = process.env.REACT_APP_GITHUB;
+  const portfolio = process.env.REACT_APP_PORTFOLIO;
+  const email = process.env.REACT_APP_EMAIL;
+
+  console.log(email);
   return (
     <div className="footer">
       {/* {footer_gradient} */}
@@ -27,7 +34,7 @@ const Footer = () => {
         <a href={angel} >
           <FaAngellist className="footer__icons" style={{ fill: '#FFFBF1' }} />
         </a>
-        <a href={`mailto:${email}`}>
+        <a href={email}>
           <FaEnvelope className="footer__icons" style={{ fill: '#FFFBF1' }} />
         </a>
       </footer>
