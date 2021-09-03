ff35943de1d8c9bc9c1d9ebb9953b51b4cc0bd96
TJ Taylor <tjtaylorjr@gmail.com>
2021-09-03 11:07:43 -0400

    add code to app.js to instruct server how to build client in production

--- app.js
+++ app.js
@@ -10,9 +10,12 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1", routes);
 app.use("*", (req, res) => res.status(404).json({error: "not found"}));
-// app.use(express.static(path.join(__dirname, 'client/build')));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/client/build/index.html'))
-// });
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 module.exports = app;
