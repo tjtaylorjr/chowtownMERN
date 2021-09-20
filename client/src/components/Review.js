4e75dd09b5b69208a25ce25a61f889f5ad728886
TJ Taylor <tjtaylorjr@gmail.com>
2021-09-20 13:52:27 -0400

    add code for fetch of secure url from backend from frontend request

--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -1,25 +1,44 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { updateReview, addReview } from '../services/restaurantServices';
 import { NavLink } from 'react-router-dom';
-import S3 from 'react-aws-s3';
+//import S3 from 'react-aws-s3';
 
 const Review = (props) => {
   let defaultReviewState = "";
+  let defaultImageState = "";
   let editing = false;
+  const fileInput = useRef(null);
 
   if (props.location.state && props.location.state.currentReview) {
     editing = true;
     defaultReviewState = props.location.state.currentReview.text
+    defaultImageState = props.location.state.currentReview.image
   }
 
   const [review, setReview] = useState(defaultReviewState);
+  const [image, setImage] = useState([]);
   const [submitted, setSubmitted] = useState(false);
 
   const handleInputChange = (event) => {
     setReview(event.target.value);
   };
 
+  const handleFileChange = async (event) => {
+    console.log(event.target.files[0]);
+    setImage(event.target.files);
+    console.log(image);
+    const res = await fetch(`/api/v1/restaurants/review`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json"
+      }
+    });
+    const url = await res.json();
+    console.log(url);
+  };
+
   const saveReview = async () => {
+    let imageFile = "";
     let data = {
       text: review,
       name: props.user.name,
@@ -64,6 +83,23 @@ const Review = (props) => {
                   name="text"
                 />
               </div>
+              {editing ? (
+                  <div>
+                    <label htmlFor="description" Edit Photo />
+                    {defaultImageState}
+                    <button>Edit</button>
+                  </div>
+              ) : (
+                <div>
+                  <label htmlFor="description"Add Photo />
+                  <input
+                  id="imageFile"
+                  type="file"
+                  accept="image/*"
+                  onChange={handleFileChange}
+                  />
+                </div>
+              )}
               <button onClick={saveReview}>
                 Submit
               </button>
