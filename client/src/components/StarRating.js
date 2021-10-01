c323a17f4177b4b0e780cee5688bc3e714316d76
TJ Taylor <tjtaylorjr@gmail.com>
2021-10-01 11:15:13 -0400

    add code to remove stars

--- client/src/components/StarRating.js
+++ client/src/components/StarRating.js
@@ -2,6 +2,16 @@ import React, { useState } from "react";
 
 const StarRating = () => {
   const [score, setScore] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  const assignScore = (event) => {
+    const rating = event.target.value;
+    if(rating === score && score !== 0 ) {
+      setScore(score - 1);
+    } else {
+      setScore(rating)
+    }
+  }
 
   return (
     <div className="star-rating">
@@ -13,8 +23,11 @@ const StarRating = () => {
           <button
             type="button"
             key={i}
-            className={i <= score ? "on" : "off"}
-            onClick={() => setScore(i)}
+            value={i}
+            className={i <= (hover || score) ? "on" : "off"}
+            onClick={assignScore}
+            onMouseEnter={() => setHover(i)}
+            onMouseLeave={() => setHover(score)}
           >
           </button>
         );
